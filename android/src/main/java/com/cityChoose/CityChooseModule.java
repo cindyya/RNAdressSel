d8d25e353860fdd184e4fcfb2bda48a2f04f43aa
jwt <abc@icloud.com>
2017-01-10 17:57:58 +0800

    init

---
+++ android/src/main/java/com/cityChoose/CityChooseModule.java
@@ -0,0 +1,45 @@
+package com.cityChoose;
+
+import android.content.Intent;
+
+import com.cityChoose.city.CityActivity;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
+
+public class CityChooseModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+  static Callback mcallback;
+  public CityChooseModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    reactContext.addActivityEventListener(this);
+  }
+
+  @Override
+  public String getName() {
+    return "CityChooseManager";
+  }
+
+  @ReactMethod
+  public void getCity(String currentCity, Callback callback){
+    mcallback=callback;
+    Intent intent=new Intent(getReactApplicationContext(), CityActivity.class);
+    intent.putExtra("currentCity",currentCity);
+    getCurrentActivity().startActivityForResult(intent,1001);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if(requestCode==1001){
+      String city = data.getStringExtra("city");
+      if(mcallback!=null){
+        mcallback.invoke(city);
+        mcallback=null;
+      }
+    }
+  }
+}
